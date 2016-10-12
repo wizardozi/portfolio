44f4e9a5f98fbda2dc5f31204bc81d24b8dc35b3
wizardozi <alexdenommee@gmail.com>
2016-10-12 16:01:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#flip").click(function(){
-        $("#panel").slideDown("slow");
-    });
+	$(".glow").hover(function(){
+		$(this).toggleClass("shadow");
+	});
 });
+
